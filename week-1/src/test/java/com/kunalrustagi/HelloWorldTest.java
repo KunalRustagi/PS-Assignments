01274e5f263a89ce8a057ac474090628f36c24af
Kunal Rustagi <kunal@Kunals-MacBook-Air.local>
2020-04-09 21:50:24 +0530

    Updated Jenkinsfile,added tests

---
+++ week-1/src/test/java/com/kunalrustagi/HelloWorldTest.java
@@ -0,0 +1,16 @@
+package com.kunalrustagi;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class HelloWorldTest
+{
+    
+    @Test
+    public void sampleTest()
+    {
+        assertTrue( true );
+    }
+}
